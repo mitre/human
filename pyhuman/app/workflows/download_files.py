8006c309029c8bde44a4fe0ce700cc5a194bf81c
Kayla Kraines <20597974+kaylakraines@users.noreply.github.com>
2022-02-08 13:16:12 -0500

    Remove unused code from original spawn_shell file

--- pyhuman/app/workflows/download_files.py
+++ pyhuman/app/workflows/download_files.py
@@ -4,7 +4,6 @@ from time import sleep
 import ssl
 import urllib.request
 
-
 from ..utility.base_workflow import BaseWorkflow
 
 
@@ -24,21 +23,13 @@ class DownloadFiles(BaseWorkflow):
     def action(self, extra=None):
         self._download_files()
 
+
+    """ PRIVATE """
     def _download_files(self):
         ssl._create_default_https_context = ssl._create_unverified_context
         file_url = 'https://imgs.xkcd.com/comics/file_transfer.png'
         urllib.request.urlretrieve(file_url, "file_downloaded.jpg")
         sleep(5)
 
-    """ PRIVATE """
 
-    # def _spawn_shell_and_quit(self):
-    #     p = subprocess.Popen(self._determine_os_shell_command(), shell=True)
-    #     sleep(5)
-    #     p.kill()
-    #
-    # @staticmethod
-    # def _determine_os_shell_command():
-    #     if sys.platform.startswith('win32') or sys.platform.startswith('cygwin'):
-    #         return 'dir'
-    #     return 'ls -la'
+
