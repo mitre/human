c64c71d808ddcc1ce28f9990c4f7c6ed73f9e44f
Dean Lawrence <dlawrence@mitre.org>
2022-04-01 15:09:53 -0400

    Small bug and formatting fixes

--- pyhuman/app/workflows/ms_paint.py
+++ pyhuman/app/workflows/ms_paint.py
@@ -1,42 +1,45 @@
 import os
-import pyautogui
-from time import sleep
+from importlib import import_module
+from time import sleep, time
 
 from ..utility.base_workflow import BaseWorkflow
+
 
 WORKFLOW_NAME = 'MicrosoftPaint'
 WORKFLOW_DESCRIPTION = 'Create a blank MS Paint file (Windows)'
 
 DEFAULT_INPUT_WAIT_TIME = 2
+DEFAULT_PAINT_PATH = paint_path = 'C:\Windows\System32\mspaint.exe'
 
 
 def load():
-    return msPaint()
+    pyautogui = import_module('pyautogui')
+    return msPaint(pyautogui=pyautogui)
 
 
 class msPaint(BaseWorkflow):
 
-    def __init__(self, input_wait_time=DEFAULT_INPUT_WAIT_TIME):
+    def __init__(self, pyautogui, input_wait_time=DEFAULT_INPUT_WAIT_TIME, paint_path=DEFAULT_PAINT_PATH):
         super().__init__(name=WORKFLOW_NAME, description=WORKFLOW_DESCRIPTION)
+
+        self.pyautogui = pyautogui
         self.input_wait_time = input_wait_time
+        self.paint_path = paint_path
 
     def action(self, extra=None):
         self._ms_paint()
 
-
     """ PRIVATE """
 
-    @staticmethod
     def _ms_paint(self):
-        paint_path = "C:\Windows\System32\mspaint.exe"
-        os.startfile(paint_path)
-        pyautogui.getWindowsWithTitle("Paint")
+        os.startfile(self.paint_path)
+        self.pyautogui.getWindowsWithTitle('Paint')
         sleep(self.input_wait_time)
-        pyautogui.hotkey('ctrl', 's')
-        file_name = int(time.time())
+        self.pyautogui.hotkey('ctrl', 's')
+        file_name = int(time())
         sleep(self.input_wait_time)
-        pyautogui.typewrite(str(file_name))
+        self.pyautogui.typewrite(str(file_name))
         sleep(self.input_wait_time)
-        pyautogui.press('enter')
+        self.pyautogui.press('enter')
         sleep(self.input_wait_time)
-        pyautogui.hotkey('alt', 'f4')
+        self.pyautogui.hotkey('alt', 'f4')
