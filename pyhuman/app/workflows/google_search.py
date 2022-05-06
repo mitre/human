abae88bfcdd16dc34a30f9e952ea88959ee8a6d7
Kayla Kraines <20597974+kaylakraines@users.noreply.github.com>
2022-05-06 12:55:30 -0400

    Expand google search to use random search from text file

--- pyhuman/app/workflows/google_search.py
+++ pyhuman/app/workflows/google_search.py
@@ -1,12 +1,13 @@
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+import os
+import random
 
 from ..utility.base_workflow import BaseWorkflow
 from ..utility.webdriver_helper import WebDriverHelper
 
-
 WORKFLOW_NAME = 'GoogleSearcher'
-WORKFLOW_DESCRIPTION = 'Search for something on Google'
+WORKFLOW_DESCRIPTION = 'Search for a random search term on Google'
 
 DEFAULT_INPUT_WAIT_TIME = 2
 
@@ -22,21 +23,35 @@ class GoogleSearch(BaseWorkflow):
         super().__init__(name=WORKFLOW_NAME, description=WORKFLOW_DESCRIPTION, driver=driver)
 
         self.input_wait_time = input_wait_time
+        self.search_list = self._load_search_list()
 
     def action(self, extra=None):
-        self._search_web('MITRE Caldera')
+        self._search_web()
 
     """ PRIVATE """
 
-    def _search_web(self, search_string):
-        self.driver.driver.get('https://www.google.com')
-        assert 'Google' in self.driver.driver.title
-        elem = self.driver.driver.find_element_by_name('q')
-        elem.clear()
-        sleep(self.input_wait_time)
-        elem.send_keys(search_string)
-        sleep(self.input_wait_time)
-        elem.send_keys(Keys.RETURN)
+    def _search_web(self):
+        random_search = self._get_random_search()
+        try:
+            self.driver.driver.get('https://www.google.com')
+            assert 'Google' in self.driver.driver.title
+            elem = self.driver.driver.find_element_by_name('q')
+            elem.clear()
+            sleep(self.input_wait_time)
+            elem.send_keys(random_search)
+            sleep(self.input_wait_time)
+            elem.send_keys(Keys.RETURN)
+        except Exception as e:
+            print('Error performing google search %s: %s' % (random_search.rstrip(), e))
 
+    def _get_random_search(self):
+        return random.choice(self.search_list)
 
-
+    @staticmethod
+    def _load_search_list():
+        wordlist = []
+        with open(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..',
+                                               'data', 'google_searches.txt')), 'r') as f:
+            for line in f:
+                wordlist.append(line)
+        return wordlist
