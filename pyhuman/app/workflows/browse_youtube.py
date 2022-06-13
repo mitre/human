d120f31bf155e1202c417a650b37e4a40d7d95f2
Kayla Kraines <20597974+kaylakraines@users.noreply.github.com>
2022-06-13 09:07:28 -0400

    Use min wait and watch times instead of hardcoded integers

--- pyhuman/app/workflows/browse_youtube.py
+++ pyhuman/app/workflows/browse_youtube.py
@@ -15,7 +15,9 @@ WORKFLOW_NAME = 'YoutubeBrowser'
 WORKFLOW_DESCRIPTION = 'Browse Youtube'
 
 DEFAULT_INPUT_WAIT_TIME = 2
+MIN_WATCH_TIME = 2 # Minimum amount of time to watch a video, in seconds
 MAX_WATCH_TIME = 150 # Maximum amount of time to watch a video, in seconds
+MIN_WAIT_TIME = 2 # Minimum amount of time to wait after searching, in seconds
 MAX_WAIT_TIME = 5 # Maximum amount of time to wait after searching, in seconds
 MAX_SUGGESTED_VIDEOS = 10
 
@@ -44,23 +46,23 @@ class GoogleSearch(BaseWorkflow):
 
         # Navigate to youtube
         self.driver.driver.get('https://www.youtube.com')
-        sleep(random.randrange(2, MAX_WAIT_TIME))
+        sleep(random.randrange(MIN_WAIT_TIME, MAX_WAIT_TIME))
 
         # Perform a youtube search
         search_element = self.driver.driver.find_element_by_css_selector('input#search') # search bar
         search_element.send_keys(random_search)
         search_element.submit()
-        sleep(random.randrange(2, MAX_WAIT_TIME))
+        sleep(random.randrange(MIN_WAIT_TIME, MAX_WAIT_TIME))
 
         # Click on a random video from the search results
         WebDriverWait(self.driver.driver, 10).until(EC.presence_of_all_elements_located((By.ID, "video-title")))
         search_results = self.driver.driver.find_elements_by_id("video-title")
         search_results[random.randrange(0, len(search_results)-1)].click()
-        sleep(random.randrange(2, MAX_WATCH_TIME))
+        sleep(random.randrange(MIN_WATCH_TIME, MAX_WATCH_TIME))
 
         # Click on a random video from the suggested videos
         for _ in range(0,random.randrange(0,MAX_SUGGESTED_VIDEOS)):
-            sleep(random.randrange(2, MAX_WAIT_TIME))
+            sleep(random.randrange(MIN_WAIT_TIME, MAX_WAIT_TIME))
             suggested_videos = self.driver.driver.find_elements_by_id("video-title")
             try:
                 suggested_videos[random.randrange(0,len(suggested_videos)-1)].click()
