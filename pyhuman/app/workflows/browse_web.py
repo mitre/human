a1adb191734afae48244f35e7fbdd827a93fe5dc
Kayla Kraines <20597974+kaylakraines@users.noreply.github.com>
2022-07-29 14:57:02 -0400

    Update navigation logic
    Instead of clicking on elements on the webpage, scrape the urls from the webpage and navigate to a random url.
    Add error handling
    Update usage of global variables
    Add a page timeout when loading a webpage

--- pyhuman/app/workflows/browse_web.py
+++ pyhuman/app/workflows/browse_web.py
@@ -5,11 +5,13 @@ from time import sleep
 from ..utility.base_workflow import BaseWorkflow
 from ..utility.webdriver_helper import WebDriverHelper
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import InvalidArgumentException
+from selenium.common.exceptions import TimeoutException
 
 WORKFLOW_NAME = 'WebBrowser'
 WORKFLOW_DESCRIPTION = 'Select a random website and browse'
 
-DEFAULT_INPUT_WAIT_TIME = 2
 MAX_NAVIGATION_CLICKS = 15
 DEFAULT_WAIT_TIME = 1
 
@@ -20,10 +22,11 @@ def load():
 
 class WebBrowse(BaseWorkflow):
 
-    def __init__(self, driver, input_wait_time=DEFAULT_INPUT_WAIT_TIME):
+    def __init__(self, driver, default_wait_time=DEFAULT_WAIT_TIME, max_navigation_clicks=MAX_NAVIGATION_CLICKS):
         super().__init__(name=WORKFLOW_NAME, description=WORKFLOW_DESCRIPTION, driver=driver)
 
-        self.input_wait_time = input_wait_time
+        self.default_wait_time = default_wait_time
+        self.max_navigation_clicks = max_navigation_clicks
         self.website_list = self._load_website_list()
 
     def action(self, extra=None):
@@ -32,17 +35,31 @@ class WebBrowse(BaseWorkflow):
     """ PRIVATE """
 
     def _web_browse(self):
+        self.driver.driver.set_page_load_timeout(45)
         random_website = self._get_random_website()
+        self._browse(random_website)
+        sleep(self.default_wait_time)
+        self._navigate_website()
+
+
+    def _get_random_website(self):
+        # Get a random website from the list of websites
+        try:
+            return random.choice(self.website_list)
+        except Exception as error:
+            print(f"Error getting random website from website list: {error}")
+
+
+    def _browse(self, random_website):
         print("Browsing to", random_website.rstrip())
         try:
             self.driver.driver.get('https://' + random_website)
-            sleep(self.input_wait_time)
-            self._navigate_webpage()
-        except Exception as e:
-            print('Error loading random website %s: %s' % (random_website.rstrip(), e))
-
-    def _get_random_website(self):
-        return random.choice(self.website_list)
+        except TimeoutException as error:
+            print(f"Timeout loading {random_website.rstrip()}: {error}")
+        except WebDriverException as error:
+            print(f"Error loading {random_website.rstrip()}: {error}")
+        except Exception as error:
+            print(f"Error loading {random_website.rstrip()}: {error}")
 
     @staticmethod
     def _load_website_list():
@@ -53,31 +70,28 @@ class WebBrowse(BaseWorkflow):
                 wordlist.append(line)
         return wordlist
 
-
-    def _navigate_webpage(self):
-        # Navigate webpage
-        navigation_clicks = random.randrange(0, MAX_NAVIGATION_CLICKS)
-        print(".... Navigating and highlighting web page", navigation_clicks, "times")
-        for _ in range(0, navigation_clicks):
+    def _navigate_website(self):
+        # Browse the currently loaded website with a random amount of clicks
+        navigation_clicks = random.randrange(0, self.max_navigation_clicks)
+        for num_click in range(1, navigation_clicks):
             clickables = self.driver.driver.find_elements(By.TAG_NAME, ("a"))
+            # If there's nothing to click, stop navigating this page
             if len(clickables) == 0:
+                print(f"... {num_click}. No clickable elements were found")
                 return
-            clickable = random.choice(clickables)
-            try:
-                self._highlight(clickable)
-                self.driver.driver.execute_script("arguments[0].target='_self';", clickable)
-                clickable.click()
-                print("........ successful navigation")
-            except:
-                print("........ X unsuccessful navigation")
-                pass
-
-    def _highlight(self, element):
-        driver = element._parent
-        def apply_style(s):
-            driver.execute_script("arguments[0].setAttribute('style', arguments[1]);",
-                                element, s)
-        original_style = element.get_attribute('style')
-        apply_style("border: 10px solid red;")
-        sleep(DEFAULT_WAIT_TIME)
-        apply_style(original_style)
+            # Navigate to a random url on the page
+            else:
+                clickable = random.choice(clickables)
+                url = clickable.get_attribute("href")
+                if url is None:
+                    print(f"... {num_click}. Invalid URL ")
+                    continue
+                try:
+                    self.driver.driver.get(url)
+                    print(f"... {num_click}. Navigated to {url}")
+                except TimeoutException as error:
+                    print(f"Timeout loading {url.rstrip()}: {error}")
+                except InvalidArgumentException as error:
+                    print(f"Error loading {url}: {error}")
+                except Exception as error:
+                    print(f"Error loading {url}: {error}")
